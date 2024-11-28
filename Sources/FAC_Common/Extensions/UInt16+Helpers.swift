0ea3e9e0ed36443f6ba619ed875368554c14d694
mikehallCD <mike@chipmunkdata.co.uk>
2024-11-28 19:55:18 +0000

    Update UInt16+Helpers.swift

--- Sources/FAC_Common/Extensions/UInt16+Helpers.swift
+++ Sources/FAC_Common/Extensions/UInt16+Helpers.swift
@@ -36,3 +36,34 @@ public extension UInt16 {
         self.init(UInt16(UInt16(msb) * UInt16(256)) + UInt16(lsb))
     }
 }
+
+public extension Int16 {
+    public func highByte() -> UInt8 {
+        return UInt8(self / 256)
+    }
+
+    public func lowByte() -> UInt8 {
+       return UInt8(self - ((self / 256) * 256))
+    }
+
+    public func hex() -> String {
+        return String(self, radix: 16).padded(size: 4)
+    }
+
+    public func bin() -> String {
+        return String(self, radix: 2).padded(size: 16)
+    }
+
+    public mutating func inc() {
+        self = self &+ 1
+    }
+
+    public mutating func dec() {
+        self = self &- 1
+    }
+    
+    public init(msb: UInt8, lsb: UInt8) {
+        self.init(Int16(Int16(msb) * Int16(256)) + Int16(lsb))
+    }
+}
+
